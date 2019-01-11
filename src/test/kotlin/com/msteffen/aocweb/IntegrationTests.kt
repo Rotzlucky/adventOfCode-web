ffdaf244b8eb4ad3f4bf1a407bada1f7996f3e6e
Marcel Steffen <marcel@talentsconnect.com>
2019-01-11 22:26:12 +0100

    enable test setup

---
+++ src/test/kotlin/com/msteffen/aocweb/IntegrationTests.kt
@@ -0,0 +1,42 @@
+package com.msteffen.aocweb
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
+    @Test
+    fun `Assert aoc page title, content and status code`() {
+        println(">> Assert blog page title, content and status code")
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Advent of Code</h1>", "calendar-grid")
+    }
+
+    @Test
+    fun `Assert Day1 page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
+
+}
